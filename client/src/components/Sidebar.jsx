086ce8a54a5d3ba2ec954be6a4af0b3599530b78
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-06-16 22:32:20 +0200

    sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { logo, sun } from '../assets';
+import {navlinks} from '../constants';
+
+const Icon = ({styles, name, imgUrl, isActive, disable, handleClick}) => (
+    <div>
+        
+    <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disable && 'cursor-pointer'} ${styles}`}>
+        {!isActive ? (
+            <img src={imgUrl} alt='fund_logo' className='w-1/2 h-1/2' />
+        ) : (
+            <img src={imgUrl} alt='fund_logo' className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
+        )}
+        
+    </div>
+    </div>
+)
 
 const Sidebar = () => {
+    const navigate = useNavigate();
+    const [isActive, setIsActive] = useState('dashboard');
   return (
-    <div>Sidebar</div>
+    <div className='flex justify-between items-center flex-col sticky top-5 h-[93vh] '>
+        
+        
+        <Link to='/'>
+            <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo}/>
+        </Link>
+    </div>
   )
 }
 
