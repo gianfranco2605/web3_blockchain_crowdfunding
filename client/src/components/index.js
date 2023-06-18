244f8cf8fa33088bad6aecd74005661dea6920e8
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-06-18 13:24:32 +0200

    Custom button done

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,5 @@
 import { default as Sidebar } from './Sidebar';
 import { default as Navbar } from './Navbar';
+import { default as CustomButtom  } from './CustomButtom'
 
-export { Sidebar, Navbar }
+export { Sidebar, Navbar, CustomButtom }
